44678a91943dc4295bf44b747d62ad51fcc4895d
kanchan raju <kanchanraju2015@gmail.com>
2026-01-16 10:55:26 +0530

    first commit

---
+++ src/main/java/com/nit/springboot_nit_employee_mgmt/SpringbootNitEmployeeMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.nit.springboot_nit_employee_mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNitEmployeeMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNitEmployeeMgmtApplication.class, args);
+	}
+
+}
